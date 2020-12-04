2325cbb35839e0e937708d8018806dcad83147ca
Kostya <konstantin.sedniov@gmail.com>
2020-12-04 18:28:05 +0300

    initial commit

---
+++ TestGenerator/FileIO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator
+{
+    class FileIO
+    {
+        public async Task<string> ReadFileAsync(string source)
+        {
+            using (var reader = new StreamReader(source))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        public async Task WriteFileAsync(string destination, List<string> files)
+        {
+            foreach (var file in files)
+            {
+                using (var writer = new StreamWriter($@"{destination}\"))
+                {
+                    await writer.WriteAsync(file);
+                }
+            }
+        }
+    }
+}
